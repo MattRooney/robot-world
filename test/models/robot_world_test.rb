a93c4f9d36255522a7abed644de9ba11357e9aa1
Matt Rooney <matthewjrooney@gmail.com>
2015-10-14 20:31:09 -0600

    Added update and delete tests.

--- test/models/robot_world_test.rb
+++ test/models/robot_world_test.rb
@@ -57,4 +57,46 @@ class RobotWorldTest < Minitest::Test
       assert_equal "Name 3", robot_3.name
     end
 
+    def test_robot_can_be_updated
+      create_robots(1)
+      RobotWorld.update(1, {:name => "Name 1",
+                            :city => "City 1",
+                            :state => "State 1",
+                            :birthday => "B-day 1",
+                            :hired => "FIRED",
+                            :department => "department 1"})
+      robot = RobotWorld.find(1)
+
+      assert_equal "FIRED", robot.hired
+    end
+
+    def test_only_the_correct_robot_is_updated
+      create_robots(10)
+      RobotWorld.update(7, {:name => "Name 7",
+                            :city => "City 7",
+                            :state => "State 7",
+                            :birthday => "B-day 7",
+                            :hired => "hired 7",
+                            :department => "deFartment"})
+      robot_7 = RobotWorld.find(7)
+      robot_8 = RobotWorld.find(8)
+
+      assert_equal "deFartment", robot_7.department
+      assert_equal "department 8", robot_8.department
+    end
+
+    def test_it_deletes
+      create_robots(12)
+      RobotWorld.delete(2)
+
+      assert_equal 11, RobotWorld.all.count
+    end
+
+    def test_it_only_deletes_correct_robot
+      create_robots(6)
+      RobotWorld.delete(4)
+
+      assert_equal ["City 1", "City 2", "City 3", "City 5", "City 6"],
+        RobotWorld.all.map { |robot| robot.city }
+    end
 end
