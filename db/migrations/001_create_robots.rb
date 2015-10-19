09c8cd772589ec8a7cb1185ab92855d97a2cc698
Matt Rooney <matthewjrooney@gmail.com>
2015-10-18 22:02:58 -0600

    Updated robot_world_test's to passing

--- db/migrations/001_create_robots.rb
+++ db/migrations/001_create_robots.rb
@@ -6,6 +6,11 @@ environments = ["test", "development"]
 environments.each do |env|
   Sequel.sqlite("db/robot_world_#{env}.sqlite3").create_table(:robots) do
     primary_key :id
-    String  :title
+    String  :name
+    String  :city
+    String  :state
+    String  :birthday
+    String  :hired
+    String  :department 
   end
 end
--- test/models/robot_world_test.rb
+++ test/models/robot_world_test.rb
@@ -3,70 +3,66 @@ require_relative '../test_helper'
 class RobotWorldTest < Minitest::Test
   def create_robots(num)
     num.times do |id|
-      id += 1
-      RobotWorld.create ({:name => "Name #{id}",
-                          :city => "City #{id}",
-                          :state => "State #{id}",
-                          :birthday => "B-day #{id}",
-                          :hired => "hired #{id}",
-                          :department => "department #{id}"})
+      RobotWorld.create ({:name => "Name #{id+1}",
+                          :city => "City #{id+1}",
+                          :state => "State #{id+1}",
+                          :birthday => "B-day #{id+1}",
+                          :hired => "Hired #{id+1}",
+                          :department => "Department #{id+1}"})
     end
   end
 
   def test_robot_is_created
     create_robots(1)
-    robot = RobotWorld.find(1)
+    robot = RobotWorld.find(RobotWorld.all.first.id)
 
-    assert_equal 1, robot.id
-    assert_equal "Pedro", robot.name
-    assert_equal "Juarez", robot.city
-    assert_equal "MX", robot.state
-    assert_equal "6/4/69", robot.birthday
-    assert_equal "9/9/99", robot.hired
-    assert_equal "sports", robot.department
+    assert_equal RobotWorld.all.first.id, robot.id
+    assert_equal "Name 1", robot.name
+    assert_equal "City 1", robot.city
+    assert_equal "State 1", robot.state
+    assert_equal "B-day 1", robot.birthday
+    assert_equal "Hired 1", robot.hired
+    assert_equal "Department 1", robot.department
   end
 
   def test_five_robots_are_created
-    skip
     create_robots(5)
 
     assert_equal 5, RobotWorld.all.count
   end
 
   def test_three_robots_have_unique_names
-    skip
     create_robots(3)
 
     assert_equal ["Name 1", "Name 2", "Name 3"], RobotWorld.all.map { |robot| robot.name }
   end
 
   def test_three_robots_have_unique_states
-    skip
     create_robots(3)
 
     assert_equal ["State 1", "State 2", "State 3"], RobotWorld.all.map { |robot| robot.state }
   end
 
   def test_it_finds_by_id
-    skip
     create_robots(5)
-    robot_3 = RobotWorld.find(3)
+    robot = RobotWorld.find(RobotWorld.all.first.id)
 
-    assert_equal 3, robot_3.id
-    assert_equal "Name 3", robot_3.name
+    assert_equal RobotWorld.all.first.id, robot.id
+    assert_equal "Name 1", robot.name
+    assert_equal "Hired 1", robot.hired
   end
 
   def test_robot_can_be_updated
-    skip
     create_robots(1)
-    RobotWorld.update(1, {:name => "Name 1",
-                          :city => "City 1",
-                          :state => "State 1",
-                          :birthday => "B-day 1",
-                          :hired => "FIRED",
-                          :department => "department 1"})
-    robot = RobotWorld.find(1)
+    RobotWorld.update(RobotWorld.all.first.id, {:name => "Name 1",
+                                                :city => "City 1",
+                                                :state => "State 1",
+                                                :birthday => "B-day 1",
+                                                :hired => "FIRED",
+                                                :department => "Department 1"})
+    robot = RobotWorld.find(RobotWorld.all.first.id)
 
+    assert_equal "B-day 1", robot.birthday
     assert_equal "FIRED", robot.hired
   end
 
@@ -78,18 +74,17 @@ class RobotWorldTest < Minitest::Test
                           :state => "State 7",
                           :birthday => "B-day 7",
                           :hired => "hired 7",
-                          :department => "deFartment"})
-    robot_7 = RobotWorld.find(7)
-    robot_8 = RobotWorld.find(8)
+                          :department => "baseball"})
+    robot_7 = RobotWorld.find()
+    robot_8 = RobotWorld.find()
 
-    assert_equal "deFartment", robot_7.department
+    assert_equal "baseball", robot_7.department
     assert_equal "department 8", robot_8.department
   end
 
   def test_it_deletes
-    skip
     create_robots(12)
-    RobotWorld.delete(2)
+    RobotWorld.delete(RobotWorld.all.first.id)
 
     assert_equal 11, RobotWorld.all.count
   end
